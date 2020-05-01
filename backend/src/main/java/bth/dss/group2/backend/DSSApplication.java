829cef8fb378ce23e31117e685f7fa5589519bf4
Timo Dittus <timo.dittus@denkbares.com>
2020-05-01 15:14:18 +0200

    Add backend skeleton and maven project/module defs

---
+++ backend/src/main/java/bth/dss/group2/backend/DSSApplication.java
@@ -0,0 +1,12 @@
+package bth.dss.group2.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSSApplication.class, args);
+	}
+}
