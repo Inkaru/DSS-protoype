983a8af6bac51c6663346a07ca34a0722e0e201d
Timo Dittus <timo.dittus@denkbares.com>
2020-05-04 23:03:13 +0200

    Move security config to own class

--- backend/src/main/java/bth/dss/group2/backend/DSSApplication.java
+++ backend/src/main/java/bth/dss/group2/backend/DSSApplication.java
@@ -8,20 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @SpringBootApplication
 public class DSSApplication {
-
-	@Configuration
-	//@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http
-					.httpBasic()
-					.and()
-					.authorizeRequests()
-					.antMatchers( "/index", "/").permitAll()
-					.anyRequest().permitAll();
-		}
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(DSSApplication.class, args);
 	}
