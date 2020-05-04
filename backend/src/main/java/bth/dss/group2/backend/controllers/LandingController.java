23dfa841758484c5c8d378b009b7594778785a12
Timo Dittus <timo.dittus@denkbares.com>
2020-05-04 15:23:04 +0200

    Serve angular's index.html on Spring tomcat server

---
+++ backend/src/main/java/bth/dss/group2/backend/controllers/LandingController.java
@@ -0,0 +1,30 @@
+package bth.dss.group2.backend.controllers;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LandingController {
+	private static final Logger logger = LoggerFactory.getLogger(LandingController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+	public String land(Locale locale, Model model) {
+			logger.info("Welcome home! the client locale is "+ locale);
+			Date date = new Date();
+			DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+			String formattedDate = dateFormat.format(date);
+			model.addAttribute("serverTime", formattedDate );
+		return "web/index.html";
+	}
+}
