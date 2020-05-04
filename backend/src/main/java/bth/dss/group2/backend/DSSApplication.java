41943cd0218f1683614147e8049cc8ccd5ecb5ab
Timo Dittus <timo.dittus@denkbares.com>
2020-05-04 15:21:48 +0200

    Add basic security allowance to permit all requests to "/" or "/index"

--- backend/src/main/java/bth/dss/group2/backend/DSSApplication.java
+++ backend/src/main/java/bth/dss/group2/backend/DSSApplication.java
@@ -2,10 +2,26 @@ package bth.dss.group2.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class DSSApplication {
 
+	@Configuration
+	//@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.httpBasic()
+					.and()
+					.authorizeRequests()
+					.antMatchers( "/index", "/").permitAll()
+					.anyRequest().permitAll();
+		}
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DSSApplication.class, args);
 	}
