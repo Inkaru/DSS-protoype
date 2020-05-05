fa36227638939d6aef4f4971bdc95a82c929d78f
Inkaru <pierreant.cab@gmail.com>
2020-05-05 16:44:15 +0200

    created projects component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,19 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AdminDashboardComponent
+    AdminDashboardComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
