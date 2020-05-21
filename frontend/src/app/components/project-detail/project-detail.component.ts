fab9045d24722da4f241cf1ed5e5f6f26b253a29
Inkaru <pierreant.cab@gmail.com>
2020-05-21 16:55:29 +0200

    fill project detail page

--- frontend/src/app/components/project-detail/project-detail.component.ts
+++ frontend/src/app/components/project-detail/project-detail.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Project} from '../../model/project';
 import {ActivatedRoute} from '@angular/router';
 import {ApiService} from '../../services/api.service';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-project-detail',
@@ -13,7 +14,8 @@ export class ProjectDetailComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private apiService: ApiService) {}
+    private apiService: ApiService,
+    private location: Location) {}
 
   ngOnInit(): void {
     console.log('init');
@@ -23,6 +25,10 @@ export class ProjectDetailComponent implements OnInit {
   getProject() {
     const id = this.route.snapshot.paramMap.get('id');
     this.apiService.getProjectById(id)
-      .subscribe(project => this.project = project);
+      .subscribe(project => this.project = project, error => this.goBack());
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
