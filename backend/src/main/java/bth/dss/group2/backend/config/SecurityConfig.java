d7f709e8f91c16fffb516a568dda8263ff2f0659
Timo Dittus <givemekebab@gmail.com>
2020-05-25 13:25:16 +0200

    Change security config

--- backend/src/main/java/bth/dss/group2/backend/config/SecurityConfig.java
+++ backend/src/main/java/bth/dss/group2/backend/config/SecurityConfig.java
@@ -31,12 +31,15 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-				.csrf().disable() // required for post method testing, remove this in production
+				.csrf()
+				.disable() // required for post method testing, remove this in production
 				.httpBasic()
 				.and()
 				.authorizeRequests()
-				.antMatchers("/api/projects/*", "/api/users/*", "/api/login/user").authenticated()
-				.antMatchers("*.bundle.*", "/index*", "/*", "/home*", "/api/login/authenticate").permitAll();
+				.antMatchers("*.bundle.*", "/index*", "/*", "/home*", "/api/login/authenticate", "/api/users/registerUser")
+				.permitAll()
+				.anyRequest()
+				.authenticated();
 
 	/*
 				.and()
