2fd075c7125f3a31dfb7c38344b85712f48c122c
Inkaru <pierreant.cab@gmail.com>
2020-05-25 13:45:30 +0200

    refactor

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {User} from '../../model/user';
 import {ApiService} from '../../services/api.service';
@@ -11,17 +11,16 @@ import {Subscription} from 'rxjs';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
-  
+export class ProfileComponent implements OnInit, OnDestroy {
 
-  ngOnInit(): void {
-    this.initProjectsForm();
-    this.projectsSubscription = this.apiService.projectsSubject.subscribe(
-      (data: any) => {
-        this.projects = data;
-      }
-    );
-    this.apiService.getAllProjects();
+  constructor(
+    private authService: AuthService,
+    private apiService: ApiService,
+    private formBuilder: FormBuilder) {
+    this.authService.currentUser.subscribe(
+      x => {
+        this.currentUser = x;
+      });
   }
 
   currentUser: User = null;
@@ -35,11 +34,24 @@ export class ProfileComponent implements OnInit {
   profileForm: FormGroup;
   error: '';
 
-  constructor(
-  private authService: AuthService,
-  private apiService: ApiService,
-  private formBuilder: FormBuilder){
-    this.authService.currentUser.subscribe(x => this.currentUser = x);
+  slideConfig = {
+    slidesToShow: 1,
+    slidesToScoll: 1,
+    nextArrow: '<div class=\'nav-btn next-slide\'></div>',
+    prevArrow: '<div class=\'nav-btn prev-slide\'></div>',
+    dots: true,
+    autoplay: true
+  };
+
+
+  ngOnInit(): void {
+    this.initProjectsForm();
+    this.projectsSubscription = this.apiService.projectsSubject.subscribe(
+      (data: any) => {
+        this.projects = data;
+      }
+    );
+    this.apiService.getAllProjects();
   }
 
 
@@ -54,73 +66,64 @@ export class ProfileComponent implements OnInit {
     });
   }
 
-    slideConfig = {
-      'slidesToShow': 1,
-      'slidesToScoll': 1,
-      'nextArrow': '<div class=\'nav-btn next-slide\'></div>',
-      'prevArrow': '<div class=\'nav-btn prev-slide\'></div>',
-      'dots': true,
-      'autoplay': true
-    };
+  initProjectsForm() {
+    this.projectsForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+  }
 
-    initProjectsForm() {
-      this.projectsForm = this.formBuilder.group({
-        name: ['', Validators.required],
-        description: ['', Validators.required]
-      });
+  onSubmitProjectsForm() {
+    const newProject = this.projectsForm.value;
+    if (this.editMode) {
+      this.apiService.updateProjects(newProject, this.indexToUpdate);
+    } else {
+      this.apiService.createProject(newProject).subscribe(data => {
+          console.log('success');
+          console.log(data);
+          this.apiService.getAllProjects();
+          // @ts-ignore
+          $('#projectsFormModal').modal('hide');
+        },
+        error => {
+          console.log(error);
+          this.error = error.error.message;
+        });
+      this.apiService.createProject(newProject);
     }
+  }
 
-    onSubmitProjectsForm() {
-      const newProject = this.projectsForm.value;
-      if (this.editMode) {
-        this.apiService.updateProjects(newProject, this.indexToUpdate);
-      } else {
-        this.apiService.createProject(newProject).subscribe(data => {
-            console.log('success');
-            console.log(data);
-            this.apiService.getAllProjects();
-            // @ts-ignore
-            $('#projectsFormModal').modal('hide');
-          },
-          error => {
-            console.log(error);
-            this.error = error.error.message;
-          });
-        this.apiService.createProject(newProject);
-      }
+  ngOnDestroy() {
+    this.projectsSubscription.unsubscribe();
+  }
+
+  resetForm() {
+    this.editMode = false;
+    this.projectsForm.reset();
+  }
+
+  onDeleteProject(index) {
+    if (confirm('Are you sure you want delete this project?')) {
+      this.apiService.deleteProjects(index);
     }
+  }
 
-    ngOnDestroy() {
-      this.projectsSubscription.unsubscribe();
-    }
+  onEditProject(project) {
+    this.editMode = true;
+    // @ts-ignore
+    $('#projectsFormModal').modal('show');
 
-    resetForm() {
-      this.editMode = false;
-      this.projectsForm.reset();
-    }
-
-    onDeleteProject(index) {
-      if (confirm('Are you sure you want delete this project?')) {
-        this.apiService.deleteProjects(index);
-      }
-    }
-
-    onEditProject(project) {
-      this.editMode = true;
-      // @ts-ignore
-      $('#projectsFormModal').modal('show');
-
-      this.projectsForm.get('name').setValue(project.name);
-      this.projectsForm.get('description').setValue(project.description);
-      const index = this.projects.findIndex(
-        (projectEl) => {
-          if (projectEl === project) {
-            return true;
-          }
+    this.projectsForm.get('name').setValue(project.name);
+    this.projectsForm.get('description').setValue(project.description);
+    const index = this.projects.findIndex(
+      (projectEl) => {
+        if (projectEl === project) {
+          return true;
         }
-      );
-      this.indexToUpdate = index;
+      }
+    );
+    this.indexToUpdate = index;
 
 
-    }
+  }
 }
