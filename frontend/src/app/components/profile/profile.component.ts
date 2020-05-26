683fddeff4000d5c48c63bf2410b5462543a9530
Inkaru <pierreant.cab@gmail.com>
2020-05-26 12:03:42 +0200

    update profile project creation / edition

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,17 +1,16 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {User} from '../../model/user';
 import {ApiService} from '../../services/api.service';
 import {Project} from '../../model/project';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit, OnDestroy {
+export class ProfileComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
@@ -20,38 +19,24 @@ export class ProfileComponent implements OnInit, OnDestroy {
     this.authService.currentUser.subscribe(
       x => {
         this.currentUser = x;
+        this.projects = x.createdProjects;
       });
+    this.currentProject = new Project();
   }
 
   currentUser: User = null;
   projects: Project[];
+
   projectsForm: FormGroup;
-  projectsSubscription: Subscription;
 
   indexToUpdate;
+  currentProject: Project;
   editMode = false;
 
-  profileForm: FormGroup;
   error: '';
-
-  slideConfig = {
-    slidesToShow: 1,
-    slidesToScoll: 1,
-    nextArrow: '<div class=\'nav-btn next-slide\'></div>',
-    prevArrow: '<div class=\'nav-btn prev-slide\'></div>',
-    dots: true,
-    autoplay: true
-  };
-
 
   ngOnInit(): void {
     this.initProjectsForm();
-    this.projectsSubscription = this.apiService.projectsSubject.subscribe(
-      (data: any) => {
-        this.projects = data;
-      }
-    );
-    this.apiService.getAllProjects();
   }
 
 
@@ -74,27 +59,32 @@ export class ProfileComponent implements OnInit, OnDestroy {
   }
 
   onSubmitProjectsForm() {
-    const newProject = this.projectsForm.value;
     if (this.editMode) {
-      this.apiService.updateProjects(newProject, this.indexToUpdate);
-    } else {
-      this.apiService.createProject(newProject).subscribe(data => {
-          console.log('success');
-          console.log(data);
-          this.apiService.getAllProjects();
+      this.apiService.updateProject(this.currentProject).subscribe(
+        response => {
+          console.log('project updated');
+          this.authService.getCurrentUser();
+          this.currentProject = new Project();
           // @ts-ignore
           $('#projectsFormModal').modal('hide');
+        }, error => {
+          console.log(error);
+        }
+      );
+    } else {
+      this.apiService.createProject(this.currentProject).subscribe(data => {
+          console.log('success');
+          console.log(data);
+          this.authService.getCurrentUser();
+          // @ts-ignore
+          $('#projectsFormModal').modal('hide');
+          this.currentProject = new Project();
         },
         error => {
           console.log(error);
           this.error = error.error.message;
         });
-      this.apiService.createProject(newProject);
     }
-  }
-
-  ngOnDestroy() {
-    this.projectsSubscription.unsubscribe();
   }
 
   resetForm() {
@@ -114,20 +104,9 @@ export class ProfileComponent implements OnInit, OnDestroy {
 
   onEditProject(project) {
     this.editMode = true;
+    this.currentProject = project;
     // @ts-ignore
     $('#projectsFormModal').modal('show');
-
-    this.projectsForm.get('name').setValue(project.name);
-    this.projectsForm.get('description').setValue(project.description);
-    const index = this.projects.findIndex(
-      (projectEl) => {
-        if (projectEl === project) {
-          return true;
-        }
-      }
-    );
-    this.indexToUpdate = index;
-
 
   }
 }
