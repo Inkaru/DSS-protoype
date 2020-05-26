16ffb39a8162600832c35fc06ab898e3077a113f
Inkaru <pierreant.cab@gmail.com>
2020-05-26 20:40:14 +0200

    market place items & filter
    fix

--- frontend/src/app/components/marketplace/marketplace.component.ts
+++ frontend/src/app/components/marketplace/marketplace.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from '../../services/api.service';
+import {MarketplaceItem} from '../../model/marketplaceItem';
 
 @Component({
   selector: 'app-marketplace',
@@ -7,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarketplaceComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private apiService: ApiService) {
+    this.currentItem = new MarketplaceItem();
   }
 
+  items: MarketplaceItem[] = [];
+  currentItem: MarketplaceItem;
+
+  type = 'ALL';
+
+  error = '';
+
+  ngOnInit() {
+    this.apiService.mpItemsSubject.subscribe(
+      (x) => {
+        this.items = x;
+      }
+    );
+    this.filterItems();
+  }
+
+  onSubmitItemForm() {
+    this.apiService.createMPItem(this.currentItem).subscribe(
+      response => {
+        console.log('created item');
+        this.currentItem = new MarketplaceItem();
+        this.filterItems();
+        // @ts-ignore
+        $('#itemFormModal').modal('hide');
+      }, error => {
+        console.log(error);
+        this.error = error.message;
+      }
+    );
+  }
+
+  filterItems() {
+    switch (this.type) {
+      case 'ALL': {
+        this.apiService.getAllMPItems();
+        break;
+      }
+      case 'OFFER': {
+        this.apiService.getOffersMPItems();
+        break;
+      }
+      case 'REQUEST': {
+        this.apiService.getRequestsMPItems();
+        break;
+      }
+    }
+  }
 }
