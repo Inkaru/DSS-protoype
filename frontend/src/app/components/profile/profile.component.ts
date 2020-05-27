7f92075f6f0f936f50d3a634d358bde1d71ca175
Inkaru <pierreant.cab@gmail.com>
2020-05-27 11:40:21 +0200

    add items to profile

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -4,13 +4,14 @@ import {User} from '../../model/user';
 import {ApiService} from '../../services/api.service';
 import {Project} from '../../model/project';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {MarketplaceItem} from '../../model/marketplaceItem';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
 
   constructor(
     private authService: AuthService,
@@ -19,26 +20,18 @@ export class ProfileComponent implements OnInit {
     this.authService.currentUser.subscribe(
       x => {
         this.currentUser = x;
-        this.projects = x.createdProjects;
       });
     this.currentProject = new Project();
+    this.currentItem = new MarketplaceItem();
   }
 
   currentUser: User = null;
-  projects: Project[];
 
-  projectsForm: FormGroup;
-
-  indexToUpdate;
   currentProject: Project;
+  currentItem: MarketplaceItem;
   editMode = false;
 
   error: '';
-
-  ngOnInit(): void {
-    this.initProjectsForm();
-  }
-
 
   updateProfile() {
     this.apiService.updateUser(this.currentUser).subscribe(response => {
@@ -48,13 +41,6 @@ export class ProfileComponent implements OnInit {
     }, error => {
       console.log(error);
       this.error = error.message;
-    });
-  }
-
-  initProjectsForm() {
-    this.projectsForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      description: ['', Validators.required]
     });
   }
 
@@ -89,7 +75,6 @@ export class ProfileComponent implements OnInit {
 
   resetForm() {
     this.editMode = false;
-    this.projectsForm.reset();
   }
 
   onDeleteProject(id) {
@@ -106,11 +91,55 @@ export class ProfileComponent implements OnInit {
     this.currentProject = project;
     // @ts-ignore
     $('#projectFormModal').modal('show');
-
   }
 
   getRandomColor() {
     const color = Math.floor(0x1000000 * Math.random()).toString(16);
     return '#' + ('000000' + color).slice(-6);
+  }
+
+  onSubmitItemForm() {
+    if (this.editMode) {
+      this.apiService.updateMPItem(this.currentItem).subscribe(
+        response => {
+          console.log('item updated');
+          this.authService.getCurrentUser();
+          this.currentItem = new MarketplaceItem();
+          // @ts-ignore
+          $('#itemFormModal').modal('hide');
+        }, error => {
+          console.log(error);
+        }
+      );
+    } else {
+      this.apiService.createMPItem(this.currentItem).subscribe(data => {
+          console.log('success');
+          console.log(data);
+          this.authService.getCurrentUser();
+          this.currentItem = new MarketplaceItem();
+          // @ts-ignore
+          $('#itemFormModal').modal('hide');
+        },
+        error => {
+          console.log(error);
+          this.error = error.error.message;
+        });
+    }
+  }
+
+  onDeleteItem(id: string) {
+    if (confirm('Are you sure you want delete this item?')) {
+      this.apiService.deleteMPItemById(id).subscribe(value => {
+        console.log('item deleted successfully');
+        this.authService.getCurrentUser();
+      }, error => console.log(error));
+    }
+  }
+
+  onEditItem(item: MarketplaceItem) {
+    this.editMode = true;
+    this.currentItem = item;
+    // @ts-ignore
+    $('#itemFormModal').modal('show');
   }
 }
