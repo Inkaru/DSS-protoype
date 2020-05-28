b48565f2ee8512e5bd11f007796380af322bbe4e
Timo Dittus <timo.dittus@denkbares.com>
2020-05-28 20:11:49 +0200

    Add temporary workaround for duplicate key execption

--- backend/src/main/java/bth/dss/group2/backend/domain/HashTag.java
+++ backend/src/main/java/bth/dss/group2/backend/domain/HashTag.java
@@ -1,14 +1,13 @@
 package bth.dss.group2.backend.domain;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "HashTag")
 public class HashTag {
 	@Id
 	private String id;
-	@Indexed(unique = true)
+
 	private String name;
 
 	public HashTag(String name) {
