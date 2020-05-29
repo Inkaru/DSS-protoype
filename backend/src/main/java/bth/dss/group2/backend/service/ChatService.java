68805dfffbe55bd766c0fd77a6acca5601cd7ab7
Timo Dittus <timo.dittus@denkbares.com>
2020-05-29 16:09:47 +0200

    Fix backend not reusing existing chat channels with the same set of participants

--- backend/src/main/java/bth/dss/group2/backend/service/ChatService.java
+++ backend/src/main/java/bth/dss/group2/backend/service/ChatService.java
@@ -45,7 +45,7 @@ public class ChatService {
 		for (String name : chatChannelDTO.getParticipantLoginNames()) {
 			participants.add(userRepository.findByLoginName(name).orElseThrow(LoginNameNotFoundException::new));
 		}
-		ChatChannel channel = chatChannelRepository.findByParticipants(participants)
+		ChatChannel channel = chatChannelRepository.findByParticipantsContains(participants)
 				.orElseGet(() -> createChatChannel(participants));
 		logger.info("CHAT CHANNEL ESTABLISHED:" + channel);
 		return ChatChannelDTO.create(channel);
