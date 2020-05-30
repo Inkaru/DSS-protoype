4a28dd3efe43e36076a4f767da1867368e3f01b4
Inkaru <pierreant.cab@gmail.com>
2020-05-30 14:48:00 +0200

    better style for homepage

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2) {
+    this.renderer.setStyle(document.body, 'background', 'linear-gradient(rgba(0, 0, 150, 0.5), rgba(0, 0, 50, 0.1)),url(\'../../../assets/bg-title.jpeg\') no-repeat center');
+    this.renderer.setStyle(document.body, 'background-size', '100%');
+  }
 
   ngOnInit(): void {
   }
